a1c6199ac482e350b3665a0ab86d3a865638ea23
ellacarlson23 <115573152+ellacarlson23@users.noreply.github.com>
2023-11-11 12:56:58 -0600

    Initial Commit

---
+++ EmojiProject/EmojiProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiProjectApp.swift
+//  EmojiProject
+//
+//  Created by Ella Carlson on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
